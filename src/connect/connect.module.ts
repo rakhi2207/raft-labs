c144dedae2b6198a28fd6a8d4d01765aacb54382
Rakhi jha <61816749+rakhi2207@users.noreply.github.com>
2024-12-02 12:49:28 +0530

    Update connect.module.ts

--- src/connect/connect.module.ts
+++ src/connect/connect.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-// import { AlertGateway } from './alert/alert.gateway';
-// import { AlertController } from './alert/alert.controller';
 import { ChatGateway } from './chat/gateway/chat.gateway';
 import { ChatController } from './chat/controllers/chat.controller';
 import { AuthModule } from 'src/auth/auth.module';
